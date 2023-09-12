8c0164d267006e087954452425c37461d5d059ad
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-12 10:32:19 -0600

    final coupon code

--- js/cupones.js
+++ js/cupones.js
@@ -5,6 +5,13 @@ const pResult = document.querySelector("#result");
 
 btn.addEventListener("click", calcularPrecioDescuento);
 
+// const arrayUObjeto = undefined;
+const couponsObj = {
+  "soy spiderman": 30,
+  me_gusta_el_pan: 25,
+  batman_es_racista: 50,
+};
+
 function calcularPrecioDescuento() {
   const price = Number(inputPrice.value);
   const coupon = inputCoupon.value;
@@ -16,18 +23,26 @@ function calcularPrecioDescuento() {
 
   let discount;
 
-  switch (coupon) {
-    case "cupon spiderman":
-      discount = 30;
-      break;
-    case "me_gusta_el_pan":
-      discount = 25;
-      break;
-    default:
-      pResult.innerText =
-        "El cupón no es válido o no aplica en tu(s) producto(s)";
-      return;
+  if (couponsObj[coupon]) {
+    discount = couponsObj[coupon];
+  } else {
+    pResult.innerText =
+      "El cupón no es válido o no aplica en tu(s) producto(s)";
+    return;
   }
+
+  // switch (coupon) {
+  //   case "cupon spiderman":
+  //     discount = 30;
+  //     break;
+  //   case "me_gusta_el_pan":
+  //     discount = 25;
+  //     break;
+  //   default:
+  //     pResult.innerText =
+  //       "El cupón no es válido o no aplica en tu(s) producto(s)";
+  //     return;
+  // }
 
   //   if (coupon == "cupon spiderman") {
   //     discount = 30;
