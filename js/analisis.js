4be1ebbe6b2fd976e080b67a924f983d499256a4
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-13 10:49:11 -0600

    projection by company

--- js/analisis.js
+++ js/analisis.js
@@ -64,3 +64,31 @@ function medianaEmpresaYear(nombre, year) {
     return StatisticsMath.calcularMediana(empresas[nombre][year]);
   }
 }
+
+function proyeccionPorEmpresa(nombre) {
+  if (!empresas[nombre]) {
+    console.warn("La empresa no existe");
+  } else {
+    const emopresaYears = Object.keys(empresas[nombre]);
+    const listaMedianaYears = emopresaYears.map((year) => {
+      return medianaEmpresaYear(nombre, year);
+    });
+    let porcentajesCrecimiento = [];
+
+    for (let i = 1; i < listaMedianaYears.length; i++) {
+      const salarioActual = listaMedianaYears[i];
+      const salarioPasado = listaMedianaYears[i - 1];
+      const crecimiento = salarioActual - salarioPasado;
+      const porcentajeCrecimiento = crecimiento / salarioPasado;
+      porcentajesCrecimiento.push(porcentajeCrecimiento);
+    }
+    const medianaPorcentajesDeCrecimiento = StatisticsMath.calcularMediana(
+      porcentajesCrecimiento
+    );
+    const ultimaMediana = listaMedianaYears[listaMedianaYears.length - 1];
+    const aumento = ultimaMediana * medianaPorcentajesDeCrecimiento;
+    const nuevoMediana = ultimaMediana + aumento;
+
+    return nuevoMediana;
+  }
+}
